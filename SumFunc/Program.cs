21b781f4c1619d2886dfcaa7c16ab397e7c963cb
Ya <teset@test.ru>
2025-05-06 18:54:46 +0300

    Add project files.

---
+++ SumFunc/Program.cs
@@ -0,0 +1,46 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        var numbers = Console.ReadLine()?
+                             .Split(", ")
+                             .Select(s => int.TryParse(s, out var num) ? num : (int?)null)
+                             .Where(num => num != null)
+                             .Select(num => num!.Value)
+                             .ToArray();
+
+        var result = SumMinNumbers(numbers);
+
+        Console.WriteLine(result);
+    }
+
+    public static int SumMinNumbers(int[] numbers)
+    {
+        if (numbers == null)
+            throw new ArgumentNullException();
+        if (numbers.Length == 0)
+            throw new ArgumentException("Пустой массив");
+        if (numbers.Length <= 2)
+            return numbers.Sum();
+
+        int firstMinNumber = Math.Min(numbers[0], numbers[1]);
+        int secondMinNumber = Math.Max(numbers[0], numbers[1]);
+
+        for (int i = 2; i < numbers.Length; i++)
+        {
+            if (numbers[i] < firstMinNumber)
+            {
+                secondMinNumber = firstMinNumber;
+                firstMinNumber = numbers[i];
+                continue;
+            }
+
+            if (numbers[i] < secondMinNumber)
+                secondMinNumber = numbers[i];
+        }
+
+        return firstMinNumber + secondMinNumber;
+    }
+
+
+}
---
+++ SumFuncTests/Tests.cs
@@ -0,0 +1,27 @@
+﻿namespace SumFuncTests
+{
+    public class Tests
+    {
+
+        [Test]
+        public void SumMinNumbers_EmptyArray_ThrowsArgumentException()
+        {
+            int[] emptyArray = Array.Empty<int>();
+            Assert.Throws<ArgumentException>(() => Program.SumMinNumbers(emptyArray));
+        }
+
+        [Test]
+        public void SumMinNumbers_NegativeNumbers_ReturnsCorrectSum()
+        {
+            int[] numbers = { -5, -4, -3, -2 };
+            int result = Program.SumMinNumbers(numbers);
+            Assert.That(result, Is.EqualTo(-9));
+        }
+
+        [Test]
+        public void SumMinNumbers_NullInput_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.SumMinNumbers(null));
+        }
+    }
+}
